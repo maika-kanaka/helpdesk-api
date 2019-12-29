f9c39c60ea6593172f21ff97525bb0d1e29967de
Maika Kanaka <maika-kanaka@protonmail.com>
2019-12-29 20:05:39 +0700

    add cors middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        // bif
+        'cors' => \App\Http\Middleware\Cors::class
     ];
 
     /**
