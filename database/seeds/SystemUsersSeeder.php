51c0a984c35c76e381cab4edb0e5a3eb5bd40bcb
Maika Kanaka <maika-kanaka@protonmail.com>
2020-08-01 12:00:26 +0700

    MAU REFRESH DATA

--- database/seeds/SystemUsersSeeder.php
+++ database/seeds/SystemUsersSeeder.php
@@ -16,6 +16,7 @@ class SystemUsersSeeder extends Seeder
                 'group_id' => 1, // root
                 'user_id' => 1,
                 'user_name' => 'maika-kanaka',
+                'user_category' => NULL,
                 'user_fullname' => 'Maika Kanaka',
                 'user_email' => 'bif@maika-kanaka.com',
                 'user_password' => password_hash('123456', PASSWORD_DEFAULT),
@@ -28,9 +29,38 @@ class SystemUsersSeeder extends Seeder
             [
                 'group_id' => 2,
                 'user_id' => 2,
-                'user_fullname' => 'Tn. Technician',
-                'user_name' => 'technician',
-                'user_email' => 'technician@maika-kanaka.com',
+                'user_fullname' => 'Tn. Technician Hardware',
+                'user_name' => 'technician_hardware',
+                'user_category' => 1,
+                'user_email' => 'technician_hardware@maika-kanaka.com',
+                'user_password' => password_hash('123456', PASSWORD_DEFAULT),
+                'is_new' => 'N',
+
+                'created_at' => '2000-01-01 00:00:00',
+                'created_by' => 0
+            ],
+
+            [
+                'group_id' => 2,
+                'user_id' => 3,
+                'user_fullname' => 'Tn. Technician Software',
+                'user_name' => 'technician_software',
+                'user_category' => 2,
+                'user_email' => 'technician_software@maika-kanaka.com',
+                'user_password' => password_hash('123456', PASSWORD_DEFAULT),
+                'is_new' => 'N',
+
+                'created_at' => '2000-01-01 00:00:00',
+                'created_by' => 0
+            ],
+
+            [
+                'group_id' => 2,
+                'user_id' => 4,
+                'user_fullname' => 'Tn. Technician Network',
+                'user_name' => 'technician_network',
+                'user_category' => 2,
+                'user_email' => 'technician_network@maika-kanaka.com',
                 'user_password' => password_hash('123456', PASSWORD_DEFAULT),
                 'is_new' => 'N',
 
@@ -40,9 +70,10 @@ class SystemUsersSeeder extends Seeder
 
             [
                 'group_id' => 3,
-                'user_id' => 3,
+                'user_id' => 5,
                 'user_fullname' => 'Ny. Requester',
                 'user_name' => 'requester',
+                'user_category' => NULL,
                 'user_email' => 'requester@maika-kanaka.com',
                 'user_password' => password_hash('123456', PASSWORD_DEFAULT),
                 'is_new' => 'N',
